75b9c144c67df4943999dfd86d81fdf9fccbc213
Sairamdondapati <sairamdondapatii@gmail.com>
2024-10-17 12:30:12 +0530

    17/10/2024

---
+++ Assignment-9/form-submission/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Form from './Components/Form'
+import ShowFormData from './Components/ShowFormData';
+
+
+const App = () => {
+  
+  let initialObj = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    phNumber: "",
+    country: "",
+    birthDate: "",
+    avatar: "",
+    marriageStatus: 'false', // default
+    gender: ""
+};
+
+const [formData,setFormData] = useState(initialObj);
+const [formState,setFormState] = useState(false)
+const [showdata,setShowData] = useState({})
+
+function handleSubmit(e){
+    e.preventDefault();
+    setFormState(true)
+    setShowData(formData)
+    setFormData(initialObj)
+}
+
+function handleChange(e){
+    const {name, value} = e.target;
+    setFormData((prevState)=>{
+        return {...prevState, [name]:value }
+    })
+}
+  return (
+    <>
+    <div className='title'>
+      <h1>React Form Assignment</h1>
+    </div>
+      <div className='form-container'>
+        <Form formData={formData} handleSubmit={handleSubmit} handleChange ={handleChange}/>
+        {formState ? <ShowFormData showdata={showdata}  /> : null}
+      </div>
+    </>
+    
+  )
+}
+
+export default App
