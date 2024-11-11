4df6c88cfa7cb09282864baa9a5cef4cde29f22f
Sairamdondapati <sairamdondapatii@gmail.com>
2024-11-11 17:43:54 +0530

    11/11/2024

---
+++ Chakra-ui/chakra-ui/src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Text,
+} from '@chakra-ui/react'
+
+const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, eum?</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default App
---
+++ Chakra-ui/chakra-ui/src/main.jsx
@@ -0,0 +1,10 @@
+import {ChakraProvider} from '@chakra-ui/react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+)
